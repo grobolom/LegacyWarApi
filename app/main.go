16a0c786d5a050f316e6994a083945f93d4c6975
Vasja Volin <vasja.volin@gmail.com>
2016-05-14 21:02:46 -0400

    add a call pulling the db

--- app/main.go
+++ app/main.go
@@ -4,6 +4,8 @@ import (
     "net/http"
     "fmt"
     "log"
+    "labix.org/v2/mgo"
+    "os"
 )
 
 func defaultHandler(w http.ResponseWriter, r *http.Request) {
@@ -17,4 +19,17 @@ func main() {
         log.Fatal("ListenAndServe: ", err)
         return
     }
+}
+
+// putting things in here for now while we figure out how to work with
+// mongo better
+
+func db() *mgo.Session {
+    session, err := mgo.Dial("legacywarapi_database_1")
+    if err != nil {
+        panic(err)
+    }
+    session.SetMode(mgo.Monotonic, true)
+    session.DB('local')
+    return session
 }
